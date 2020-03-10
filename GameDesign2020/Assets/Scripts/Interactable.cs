2be8f5fc823a76c1fb6be0b5ea9db5a98d5b9e08
hutchid6 <47127694+hutchid6@users.noreply.github.com>
2020-03-09 20:42:56 -0400

    Initial Commit

---
+++ GameDesign2020/Assets/Scripts/Interactable.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Serialization;
+/// attached to the non-player characters, and stores the name of the
+/// Yarn node that should be run when you talk to them.
+namespace Yarn.Unity.Example
+{
+    public class Interactable : MonoBehaviour
+    {
+
+        public string characterName = "";
+
+        [FormerlySerializedAs("startNode")]
+        public string talkToNode = "";
+
+        [Header("Optional")]
+        public YarnProgram scriptToLoad;
+
+        // Use this for initialization
+        void Start()
+        {
+            if (scriptToLoad != null)
+            {
+                DialogueRunner dialogueRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
+                dialogueRunner.Add(scriptToLoad);
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
---
+++ GameDesign2020/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+using Yarn.Unity.Example;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rb;
+    Transform playerT;
+    Animator playerAnim;
+    float speed = 0.25f;
+    float interactionRadius = 2.0f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerT = GetComponent<Transform>();
+        playerAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+        {
+            playerT.Translate(new Vector3(1, 1, 0) * speed);
+        }
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+        {
+            playerT.Translate(new Vector3(-1, 1, 0) * speed);
+        }
+        else if (Input.GetKey("s") && Input.GetKey("a"))
+        {
+            playerT.Translate(new Vector3(-1, -1, 0) * speed);
+        }
+        else if (Input.GetKey("s") && Input.GetKey("d"))
+        {
+            playerT.Translate(new Vector3(1, -1, 0) * speed);
+        }
+        else if(Input.GetKey("w"))
+        {
+            playerT.Translate(new Vector3(0, 1, 0) * speed);
+        }
+        else if (Input.GetKey("d"))
+        {
+            playerT.Translate(new Vector3(1, 0, 0) * speed);
+            playerAnim.Play("playerWalkRight");
+        }
+        else if (Input.GetKey("s"))
+        {
+            playerT.Translate(new Vector3(0, -1, 0) * speed);
+        }
+        else if (Input.GetKey("a"))
+        {
+            playerT.Translate(new Vector3(-1, 0, 0) * speed);
+        }
+        else
+        {
+            playerAnim.Play("playerIdle");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CheckForNearbyNPC();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CheckForNearbyNPC();
+        }
+    }
+
+    public void CheckForNearbyNPC()
+    {
+        var allParticipants = new List<Interactable>(FindObjectsOfType<Interactable>());
+        var target = allParticipants.Find(delegate (Interactable p) {
+            return string.IsNullOrEmpty(p.talkToNode) == false && // has a conversation node?
+            (p.transform.position - transform.position).magnitude <= interactionRadius;//is in range?
+        });
+        if (target != null)
+        {
+            // Kick off the dialogue at this node.
+            FindObjectOfType<DialogueRunner>().StartDialogue(target.talkToNode);
+        }
+    }
+}
