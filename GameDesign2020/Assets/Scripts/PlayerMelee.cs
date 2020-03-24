283afe28cad93785a829413a0db532d26fb0c7fc
Joshua Shoenfelt <43758648+shoneyjosh@users.noreply.github.com>
2020-03-24 16:09:57 -0400

    Melee_added
    Added melee

---
+++ GameDesign2020/Assets/Scripts/PlayerMelee.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMelee : MonoBehaviour{
+
+    public GameObject sword;
+
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P)){
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        // Play an attack animation
+        Debug.Log("SLASH");
+        // Detect all enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        // Apply damage
+        foreach(Collider2D enemy in hitEnemies){
+        Debug.Log("We hit " + enemy.name);
+        }
+
+    }   
+    void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+}
+
+
