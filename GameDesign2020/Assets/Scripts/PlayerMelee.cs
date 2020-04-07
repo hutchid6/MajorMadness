41cead6a6e35e6e615c90bc441d68c9297838fd9
ShoneyJosh <43758648+shoneyjosh@users.noreply.github.com>
2020-04-07 14:54:41 -0400

    Cooldown PlayerMelee

--- GameDesign2020/Assets/Scripts/PlayerMelee.cs
+++ GameDesign2020/Assets/Scripts/PlayerMelee.cs
@@ -8,14 +8,32 @@ public class PlayerMelee : MonoBehaviour{
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
 
+    public GameObject sword;
+    float timeOfLastShot = 1f; //Cooldown
 
+
+    private void Start() {
+        sword.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
+        
+        //Increase value of time since last attack
+        timeOfLastShot+= Time.deltaTime;
+
         //Attack on 'P', can be changed to any button
-        if(Input.GetKeyDown(KeyCode.P)){
+        //Check for cooldown
+        if (Input.GetKey(KeyCode.P) && timeOfLastShot>1f){
             Attack();
+            //reset cooldown
+            timeOfLastShot = 0f;
+            //show sword, after two seconds, call Disappear() and make it disappear
+            sword.SetActive(true);
+            Invoke("Disappear", 1);
         }
+
+        
     }
 
 
@@ -30,7 +48,7 @@ public class PlayerMelee : MonoBehaviour{
         Debug.Log("We hit " + enemy.name);
         }
 
-    }   
+    }
 
     //Shows circle when Player object is selected, allows for it to show the range of attack based on attackPoint object
     void OnDrawGizmosSelected()
@@ -40,7 +58,15 @@ public class PlayerMelee : MonoBehaviour{
 
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
+    
+    //Make sword disappear
+    void Disappear() {
+        sword.SetActive(false);
+    }
 
 }
+
+
+
 
 
